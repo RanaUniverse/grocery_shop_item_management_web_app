d276a8b4be5802f965b6ac4d99ffac6f6507a5f7
RanaUniverse <RanaUniverse321@gmail.com>
2026-04-20 18:25:15 +0530

    now i added flask library and running main.py can start a local development server

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from grocery-shop-item-management-web-app!")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index_page():
+    return "<h1>Rana Universe</h1>"
 
 
 if __name__ == "__main__":
-    main()
+
+    # This is running a demo development server
+    app.run(
+        host="0.0.0.0",
+        port=9999,
+        debug=True,
+    )
