75d745e84e4aa54c28649889d2a930b9e23cc7f9
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-03 21:10:12 +0530

    now i can easily switch to postgres or sqlite just chnage one variable at to pof the db_make.py module

--- db_codes/db_make.py
+++ db_codes/db_make.py
@@ -9,19 +9,30 @@ from sqlmodel import (
     create_engine,
 )
 
+# if i will want to use postgres i will use true else
+# if i want to use sqlite i will use false
+IS_USING_POSTGRES: bool = True
+
+
 sqlite_file_name = "zzz_database.db"
-DATABASE_URL = f"sqlite:///{sqlite_file_name}"
+SQLITE_DATABASE_URL = f"sqlite:///{sqlite_file_name}"
 
 
-# DB_USERNAME = "rana"
-# DB_PASSWORD = "abc"
-# DB_HOST = "localhost"
-# DB_PORT = "5432"
-# DB_NAME = "grocery_db"
+DB_USERNAME = "rana"
+DB_PASSWORD = "abc"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "g"
 
-# DATABASE_URL = (
-#     f"postgresql://{DB_USERNAME}:{DB_PASSWORD}" f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# )
+POSTGRES_DATABASE_URL = (
+    f"postgresql://{DB_USERNAME}:{DB_PASSWORD}" f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+
+if IS_USING_POSTGRES:
+    DATABASE_URL = POSTGRES_DATABASE_URL
+else:
+    DATABASE_URL = SQLITE_DATABASE_URL
 
 
 engine = create_engine(url=DATABASE_URL)
